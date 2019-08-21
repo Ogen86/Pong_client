d4d5633143572ee3442e043de377a760cb188f44
Ogen86 <ogen86@gmail.com>
2019-08-21 10:59:29 +0200

    first halfway done part

---
+++ src/application/LoginController.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	
+	@FXML
+	MenuItem exitButton;
+	
+	@FXML
+	Label loginLabel;
+	
+	@FXML
+	TextField hostField;
+	
+	@FXML
+	TextField usernameField;
+	
+	@FXML
+	Button loginButton;
+
+}
